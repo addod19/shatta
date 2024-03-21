01949033694bc7feb5feb17f92cd519c7e70d7b3
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2024-03-21 08:25:23 +0000

    Add graphql, graphiql-rails to gem file, seed database and migrate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+
+
+
+
+Shatta = Artist.create!(
+    email: "shatta.wale@1don.com",
+    first_name: "Charles",
+    last_name: "Nii Armah"
+)
+
+
+Item.create!(
+    [
+        {
+            title: "Already",
+            description: "The Lion King",
+            artist: Shatta,
+            image_url: "https://images.app.goo.gl/6G6MiQzLNc6eS1sc9"
+        },
+        {
+            title: "Kakai",
+            description: "After The Storm",
+            artist: Shatta,
+            image_url: "https://images.app.goo.gl/Xc7ZqantmPiZiTms8"
+        },
+        {
+            title: "Taking Over",
+            description: "Taking Over",
+            artist: Shatta,
+            image_url: "https://images.app.goo.gl/ZoBsoP6rvYNNYJB39"
+        }
+    ]
+)
+
+
+
+
+
+
+
+
+
